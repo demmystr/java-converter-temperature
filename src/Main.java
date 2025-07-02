cf7a0414a2cbc615c51513e1820091a9857e7029
mikhail.smagin <mikhail.smagin@vegatele.com>
2025-07-02 18:38:13 +0300

    add celsius to fahrenheit converter

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,15 @@ public class Main {
         double fahrenheit = 100.0;
         double celsius = f2c(fahrenheit);
         System.out.printf("%s fahrenheit = %s celsius\n", fahrenheit, celsius);
+        fahrenheit = c2f(celsius);
+        System.out.printf("%s celsius = %s fahrenheit\n",  celsius, fahrenheit);
     }
 
     public static double f2c(double fahrenheit) {
         return (fahrenheit - 32) * 5/9;
+    }
+
+    public static double c2f(double celsius) {
+        return (celsius * 9/5) + 32;
     }
 }
