4c10b5a1feaab0d1d14baca0636f087cf5b63fa7
mikhail.smagin <mikhail.smagin@vegatele.com>
2025-07-02 18:33:51 +0300

    add fahrenheit to celsius converter

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Celsius Fahrenheit converter");
+        double fahrenheit = 100.0;
+        double celsius = f2c(fahrenheit);
+        System.out.printf("%s fahrenheit = %s celsius\n", fahrenheit, celsius);
+    }
+
+    public static double f2c(double fahrenheit) {
+        return (fahrenheit - 32) * 5/9;
     }
 }
